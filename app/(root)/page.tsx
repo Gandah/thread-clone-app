fbbbe84cea7149519671e7d8f6ecf29d9896be05
ganderson <gandahkelvin33@gmail.com>
2024-01-29 02:10:46 +0000

    another component update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,55 +1,61 @@
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import { UserButton, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import ThreadCard from "@/components/cards/ThreadCard";
-// import { Key } from "react";
+import Pagination from "@/components/shared/Pagination";
 
-// interface PostProps {
-//   _id: Key | null | undefined;
-//   parentId: string | null;
-//   text: string;
-//   author: { name: string; image: string; id: string; };
-//   commmunity: { id: string; name: string; image: string; } | null;
-//   createdAt: string;
-//   children: { author: { image: string; }; }[];
-// }
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
-export default async function Home() {
-
-  const results = await fetchPosts(1, 30);
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
+  if (!user) return null;
 
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
     <>
-      {/* <UserButton afterSignOutUrl="/"/> */}
-      <h1 className="head-text text-left">Home</h1>
+      <h1 className='head-text text-left'>Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10">
-        {results.posts.length === 0 ? (
-          <p className="no-result">No threads found</p>
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
         ) : (
           <>
-            {results.posts.map((post) => (
-              
+            {result.posts.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id || ""}
+                currentUserId={user.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
-                community={post.commmunity}
+                community={post.community}
                 createdAt={post.createdAt}
                 comments={post.children}
               />
-            )
-
-            )}
+            ))}
           </>
         )}
-
       </section>
+
+      <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </>
-  )
+  );
 }
+
+export default Home;
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,57 +1,62 @@
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-import { redirect } from "next/navigation";
 import UserCard from "@/components/cards/UserCard";
 import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-const Page = async () => {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
-  const userInfo = await fetchUser(user.id)
-  if (!userInfo?.onboarded) redirect('/onboarding');
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const results = await fetchUsers({
+  const result = await fetchUsers({
     userId: user.id,
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
-  })
+  });
 
   return (
     <section>
-      <h1 className="head-text mb-10">Search</h1>
+      <h1 className='head-text mb-10'>Search</h1>
 
-      {/* Search bar */}
-      <div className='mt-5'>
-        <Searchbar routeType='communities' />
-      </div>
+      <Searchbar routeType='search' />
 
-      <div className="mt-14 flex-col flex gap-9">
-        {results.users.length === 0 ? (
-          <p className="no-result">no users</p>
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
         ) : (
           <>
-            {results.users.map((foundUser) => {
-              return (
-                <UserCard
-                  key={foundUser.id}
-                  id={foundUser.id}
-                  name={foundUser.name}
-                  username={foundUser.username}
-                  imgUrl={foundUser.image}
-                  foundUserType='User'
-                />
-              );
-            })}
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                foundUserType='User'
+              />
+            ))}
           </>
-        )
-        }
+        )}
       </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
-  )
+  );
 }
 
-export default Page
+export default Page;
